1f274be72558da7bfed5801f816645d05d2608b6
anepretimovdiasoft <94528503+anepretimovdiasoft@users.noreply.github.com>
2023-04-13 15:16:24 +0300

    add xml

--- app/src/main/java/com/samsung/cameraxandzxing/MainActivity.java
+++ app/src/main/java/com/samsung/cameraxandzxing/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.samsung.cameraxandzxing.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+
+        
+
     }
 }
